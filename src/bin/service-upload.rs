744275e52e2717ef7cf517366113997d8f6edf38
Andrew Yourtchenko <ayourtch@gmail.com>
2019-12-24 19:34:10 +0000

    set file upload limit and pretty-print the results

--- src/bin/service-upload.rs
+++ src/bin/service-upload.rs
@@ -11,7 +11,7 @@ fn main() {
     // We start with a basic request handler chain.
     let mut chain = Chain::new(|req: &mut Request| {
         if let Some(entries) = req.extensions.get::<Entries>() {
-            Ok(Response::with(format!("{:?}", entries)))
+            Ok(Response::with(format!("{:#?}", entries)))
         } else {
             let contents = std::fs::read_to_string("upload_form.html").unwrap();
             let mut resp = Response::with(contents);
@@ -23,7 +23,7 @@ fn main() {
     // `Intercept` will read out the entries and place them as an extension in the request.
     // It has various builder-style methods for changing how it will behave, but has sane settings
     // by default.
-    chain.link_before(Intercept::default());
+    chain.link_before(Intercept::default().file_size_limit(64000000));
 
     let port = 4243;
     let bind_ip = std::env::var("BIND_IP").unwrap_or("127.0.0.1".to_string());
